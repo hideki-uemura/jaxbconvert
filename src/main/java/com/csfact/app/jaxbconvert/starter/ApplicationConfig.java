983b5e38ed6d53eba36e706dce8bbfa760e0e28a
hideki-uemura <h-uemura@cs-fact.com>
2016-05-05 19:42:11 +0900

    a

---
+++ src/main/java/com/csfact/app/jaxbconvert/starter/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package com.csfact.app.jaxbconvert.starter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+public class ApplicationConfig {
+
+	
+	
+	
+}
---
+++ src/main/java/com/csfact/app/jaxbconvert/starter/AssuredInitalizeController.java
@@ -0,0 +1,24 @@
+package com.csfact.app.jaxbconvert.starter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/jaxbconvert")
+public class AssuredInitalizeController {
+
+	@RequestMapping("/index")
+	@ResponseBody
+	public String index() {
+		return "aaa";
+	}
+
+	@RequestMapping("/execute")
+	@ResponseBody
+	public String init1() {
+		return "RESPONSE_BODY124444";
+	}
+
+
+}
